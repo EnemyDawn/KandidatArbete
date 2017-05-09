71efccb14b3b6c4afc1417bfc3d402b8aff904ca
Max Larsson <maxen940393@gmail.com>
2017-05-09 10:42:37 +0100

    Nytt intro

--- BoidTest/Game1.cs
+++ BoidTest/Game1.cs
@@ -54,7 +54,7 @@ namespace BoidTest
 
         VariableSet[] sets;
         int currentSet = 0;
-
+        Texture2D fill;
         TestPart testPart;
 
         Vector2 windowSize = new Vector2(1920, 1080);
@@ -71,10 +71,21 @@ namespace BoidTest
         CompareVid compareVid;
         string resultString;
 
-        string introText;
-
         bool[] donepart;
         bool showStats;
+
+        #region 
+
+        string genderChosen = "";
+        Rectangle boyRec;
+        Rectangle girlRec;
+
+        int age = 18;
+
+        Rectangle[]  experienceRecs;
+        int chosenExperience = -1;
+
+        #endregion
 
         public Game1()
         {
@@ -96,6 +107,8 @@ namespace BoidTest
             testPart = TestPart.intro;
 
             LoadBoids();
+            fill = new Texture2D(GraphicsDevice, 1, 1);
+            fill.SetData(new Color[] { Color.White });
 
             this.feed = new List<Feed>();
             this.obst = new List<Obst>();
@@ -109,7 +122,16 @@ namespace BoidTest
 
             this.font = Content.Load<BitmapFont>("BIG");
 
-            
+            int startPos = 400;
+            experienceRecs = new Rectangle[5]
+            {
+                new Rectangle(startPos+50,700,100,200),
+                new Rectangle(startPos+160,700,100,200),
+                new Rectangle(startPos+270,700,100,200),
+                new Rectangle(startPos+380,700,100,200),
+                new Rectangle(startPos+490,700,100,200),
+            };
+
             sets = new VariableSet[]
             {
                 new VariableSet(20,200),
@@ -123,15 +145,8 @@ namespace BoidTest
                 new VariableSet(40,50),
             };
 
-            introText = "In this case study we study the boid algorithm and try to make is as \nrealistic as possible."+
-                        "In this form you will \nbe shown multiple instances of a flock and at the same time \n" +
-                        "choose wich one of the videos you find the most realistic.\n" +
-                        "Please understand that you can quit the test at anytime if you wish,\nif so; no personal data will be saved.\n" +
-
-                       "Thank you for taking the time to participate in this study.\n" +
-                       "Best wishes\n" +
-                        "Sebastian Lundgren\n" +
-                        "Max Larsson\n";
+            boyRec = new Rectangle(300,140,100,60);
+            girlRec = new Rectangle(1000, 140, 100, 60);
 
             base.Initialize();
         }
@@ -155,35 +170,60 @@ namespace BoidTest
                 Exit();
             if (testPart == TestPart.intro)
             {
-                if (Keyboard.GetState().IsKeyDown(Keys.Enter) && lastState != Keyboard.GetState())
+                Rectangle mouseRec = new Rectangle(Mouse.GetState().X, Mouse.GetState().Y,1,1);
+
+                if (mouseRec.Intersects(boyRec) && Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    genderChosen = "Boy";
+
+                if (mouseRec.Intersects(girlRec) && Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    genderChosen = "Girl";
+
+                if (Keyboard.GetState().IsKeyDown(Keys.E) && lastState != Keyboard.GetState())
+                    age--;
+
+                if (Keyboard.GetState().IsKeyDown(Keys.R) && lastState != Keyboard.GetState())
+                    age++;
+
+                for(int n = 0;n<experienceRecs.Length;n++)
                 {
+                    if (mouseRec.Intersects(experienceRecs[n]) && Mouse.GetState().LeftButton == ButtonState.Pressed)
+                        chosenExperience = n;
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Enter) && lastState != Keyboard.GetState()  && genderChosen != "" && chosenExperience != -1)
+                {
+                    chosenExperience++;
+                    resultString += "Gender: " + genderChosen + "\n";
+                    resultString += "Age: " + age + "\n";
+                    resultString += "Media Experience: " + chosenExperience + "\n";
+
                     NextPart();
                 }
             }
             else if (testPart == TestPart.freeMode)
             {
-                if (Keyboard.GetState().IsKeyDown(Keys.Y))
+                if (Keyboard.GetState().IsKeyDown(Keys.Y) && keepDistance>0)
                     keepDistance--;
                 if (Keyboard.GetState().IsKeyDown(Keys.U) && keepDistance < visibalDistance - 10)
                     keepDistance++;
 
                 if (Keyboard.GetState().IsKeyDown(Keys.H) && keepDistance < visibalDistance - 10)
                     visibalDistance--;
-                if (Keyboard.GetState().IsKeyDown(Keys.J))
+                if (Keyboard.GetState().IsKeyDown(Keys.J) && visibalDistance<140)
                     visibalDistance++;
 
-                if (Keyboard.GetState().IsKeyDown(Keys.O) && lastState != Keyboard.GetState())
-                {
-                    if (currentSet > 0)
-                        currentSet--;
-                    SetCurrentSet();
-                }
-                if (Keyboard.GetState().IsKeyDown(Keys.P) && lastState != Keyboard.GetState())
-                {
-                    if (currentSet < sets.Length - 1)
-                        currentSet++;
-                    SetCurrentSet();
-                }
+                //if (Keyboard.GetState().IsKeyDown(Keys.O) && lastState != Keyboard.GetState())
+                //{
+                //    if (currentSet > 0)
+                //        currentSet--;
+                //    SetCurrentSet();
+                //}
+                //if (Keyboard.GetState().IsKeyDown(Keys.P) && lastState != Keyboard.GetState())
+                //{
+                //    if (currentSet < sets.Length - 1)
+                //        currentSet++;
+                //    SetCurrentSet();
+                //}
 
                 //if (Keyboard.GetState().IsKeyDown(Keys.N) && amountOfFish > 2)
                 //    amountOfFish--;
@@ -361,18 +401,44 @@ namespace BoidTest
                 {
                     spriteBatch.DrawString(font, "KeepDistance:" + keepDistance + " Y- U+", new Vector2(0, 0), Color.White);
                     spriteBatch.DrawString(font, "ViewDistance: " + visibalDistance + " H- J+", new Vector2(0, 60), Color.White);
-                    spriteBatch.DrawString(font, "Current Set:" + currentSet + " O- P+", new Vector2(0, 120), Color.White);
+                   // spriteBatch.DrawString(font, "Current Set:" + currentSet + " O- P+", new Vector2(0, 120), Color.White);
                     //spriteBatch.DrawString(font, "Fish Amount:" + amountOfFish + " N- M+", new Vector2(0, 300), Color.White);
                     
                     spriteBatch.DrawString(font, "Hide text with Tab", new Vector2(0, 180), Color.White);
                 }
-
+              
 
             }
             #endregion
             else if (testPart == TestPart.intro)
             {
-                spriteBatch.DrawString(font, introText, new Vector2(0, 0), Color.White);
+               // spriteBatch.DrawString(font, introText, new Vector2(0, 0), Color.White);
+
+                spriteBatch.DrawString(font, "Boy or Girl?", new Vector2(0, 0), Color.White);
+                spriteBatch.DrawString(font, "Boy", new Vector2(boyRec.X, boyRec.Y), Color.White);
+                spriteBatch.DrawString(font, "Girl", new Vector2(girlRec.X, girlRec.Y), Color.White);
+
+                if(genderChosen == "Boy")
+                    spriteBatch.DrawString(font, "Boy", new Vector2(boyRec.X, boyRec.Y), Color.Red);
+                else if (genderChosen == "Girl")
+                    spriteBatch.DrawString(font, "Girl", new Vector2(girlRec.X, girlRec.Y), Color.Red);
+
+                spriteBatch.DrawString(font, "Age?     Adjust with 'E' and 'R' ", new Vector2(0, 300), Color.White);
+
+
+                spriteBatch.DrawString(font, "Age: " + age, new Vector2(300, 400), Color.White);
+
+                spriteBatch.DrawString(font, "On a scale of 1-5, how experienced are you with games or digital media?", new Vector2(0, 500), Color.White);
+
+                for(int n = 0;n< experienceRecs.Length;n++)
+                {
+                    if(chosenExperience == n)
+                        spriteBatch.Draw(fill, experienceRecs[n], Color.Red);
+                    else
+                    spriteBatch.Draw(fill,experienceRecs[n], Color.White);
+                    spriteBatch.DrawString(font, (n+1).ToString(), new Vector2(experienceRecs[n].X + 28, experienceRecs[n].Y + 70), Color.Black);
+
+                }
 
             }
             else if (testPart == TestPart.Seperation5)
@@ -412,7 +478,7 @@ namespace BoidTest
             }
             else if (testPart == TestPart.nrOfTest)
             {
-                spriteBatch.DrawString(font, "Which school of fish do you prefer?", new Vector2(0, 0), Color.White);
+                spriteBatch.DrawString(font, "Which school of fish size do you prefer?", new Vector2(0, 0), Color.White);
 
                 compareVid.Draw(spriteBatch);
             }
